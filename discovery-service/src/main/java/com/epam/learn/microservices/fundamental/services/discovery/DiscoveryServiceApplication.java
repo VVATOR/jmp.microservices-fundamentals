95d45200af249e5d436fcfe89a660e4a011473b8
Vitali Vikhliayeu <Vitali_Vikhliayeu@epam.com>
2024-02-29 21:03:48 +0200

    module-5: service discovery: Added Eureka Discovery service

---
+++ discovery-service/src/main/java/com/epam/learn/microservices/fundamental/services/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.epam.learn.microservices.fundamental.services.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
