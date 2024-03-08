863f224e5f28ab1915361afa0e4607e1e1e8a7a6
Vitali Vikhliayeu <Vitali_Vikhliayeu@epam.com>
2024-03-08 05:07:24 +0200

    module-3: Clean comments

--- song-service-parent/song-service/src/test/java/com/epam/learn/microservices/fundamental/services/song/bdd/cucumber/E2ECucumberTestConfiguration.java
+++ song-service-parent/song-service/src/test/java/com/epam/learn/microservices/fundamental/services/song/bdd/cucumber/E2ECucumberTestConfiguration.java
@@ -7,16 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-//@Suite
-//@IncludeEngines("cucumber")
-//@SelectClasspathResource("com/koushick/BDD/Steps")
-//@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/testcases/searchGoogle.feature")
-//@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "com/koushick/BDD/Steps")
-//@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@googleSearch")
-//@ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME,value = "false")
-//@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
-
-
 @Configuration
 @ComponentScan(basePackages = "com.epam.learn.microservices.fundamental.services.song")
 @EnableJpaRepositories({"com.epam.learn.microservices.fundamental.services.song.repositories"})
